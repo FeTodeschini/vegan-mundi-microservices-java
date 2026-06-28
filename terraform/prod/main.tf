672ea6225257b5ada7a687748eb46e7ea296e113
Felipe Todeschini <phelipe.tod@gmail.com>
2026-06-28 13:10:51 -0400

    Adjusted terraform for ECS destroy to not destroy RDS as well

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -96,7 +96,8 @@ module "rds" {
   environment               = var.environment
   vpc_id                    = module.vpc.vpc_id
   subnet_ids                = module.vpc.public_subnet_ids
-  allowed_security_group_id = var.allow_ecs_to_rds_ingress ? module.ecs.ecs_instance_security_group_id : null
+  # Keep prod RDS independent from ECS so targeted ECS destroy does not pull RDS into the destroy graph.
+  allowed_security_group_id = null
   allowed_cidr_blocks       = var.db_allowed_cidr_blocks
 
   db_name                 = var.db_name
